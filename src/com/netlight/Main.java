a1b8c746c82df09e2d777944a7b4f4a542fddbb0
syberflow <jasoness@yandex.ru>
2020-09-07 14:55:05 +0300

    Done

--- src/com/netlight/Main.java
+++ src/com/netlight/Main.java
@@ -1,69 +1,85 @@
 package com.netlight;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.Random;
+class Shape {
+    @Override
+    public String toString() {
+        System.out.println("why!");
+        return "Shape{" +
+                "field1=" + field1 +
+                ", name='" + name + '\'' +
+                ", is_done=" + is_done +
+                ", age=" + age +
+                '}';
+    }
+
+    int field1;
+    String name;
+    boolean is_done;
+    int age;
+}
+
+class Dog {
+    String name;
+    int age;
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int humanize() {
+        return age*7;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
+
+class DogNursery{
+    Dog[] beds = new Dog[10];
+    int last_bed;
+
+    public DogNursery(){
+        int last_bed = 0;
+    }
+
+    public void add(Dog dog){
+        beds[last_bed] = dog;
+        last_bed++;
+        System.out.println("success");
+    }
+}
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("1.");
 
-        int[] arr = {34, 53, 63, 4190, 93};
-        int x;
-        int sum = 0;
-        int len = arr.length-1;
-        for (x = 0;x<len; x++)
-            sum+=arr[x];
-        while (x<len)
-            sum+=arr[x];
-        do sum+=arr[x];
-        while (x<len);
+        Dog Sparky = new Dog("Sparky");
+        Sparky.setAge(5);
+        System.out.println(Sparky.humanize());
 
-        System.out.println(sum/3);
-
-        System.out.println("2.");
-
-        System.out.println("args length " + args.length);
-        for (x = 0;x<args.length-1; x++)
-            System.out.println(args[x]);
-
-        System.out.println("3.");
-
-        System.out.print("1/1");
-        for (x = 2;x<11; x++){
-            System.out.print(" + 1/"+x);
-        }
-        System.out.println("");
-        System.out.println("4.");
-
-        int[] randarr = new int[10];
-
-        // can relate
-        Random R = new Random();
-        R.nextInt();
-
-        for (x = 0;x<randarr.length; x++)
-            randarr[x] = 0 + (int) (Math.random() * 100);
-        for (int y: randarr)
-            System.out.print(y+" ");
-        System.out.println("");
-        Arrays.sort(randarr);
-
-        for (int y: randarr)
-            System.out.print(y+" ");
-
-        System.out.println("");
-        System.out.println("5.");
-
-        System.out.println(fact(10));
-
-    }
-    public static int fact(int n) {
-        int result = 1;
-        for (int x = 1; x <= n; x++)
-            result = result * x;
-        return result;
+        DogNursery nurs = new DogNursery();
+        nurs.add(Sparky);
     }
 }
 
