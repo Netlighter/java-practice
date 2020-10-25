f90d76298735d2bb5528b9d8a5b90f7bb9ef1935
syberflow <jasoness@yandex.ru>
2020-10-25 22:27:00 +0300

    Fix 14 again

--- src/com/netlight/lab14/Main.java
+++ src/com/netlight/lab14/Main.java
@@ -1,5 +1,6 @@
 package com.netlight.lab14;
 
+import java.util.HashMap;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -15,32 +16,51 @@ public class Main {
             rules[i] = s.nextLine();
         }
         String text = s.nextLine();
-        String result;
+        String textPiece;
+        String textCopy;
         String[] src = new String[rlen];
         String[] ptr = new String[rlen];
 
         // w/o regex
-        result = text;
+        textPiece = "";
+        textCopy = text;
+
         for (int i = 0; i < rlen; i++) {
             src[i] = rules[i].split(" ")[0];
             ptr[i] = rules[i].split(" ")[1];
-
-            if (result.contains(src[i])) result = result.
-                    replaceAll(src[i], " " + ptr[i] + " ");
         }
-        System.out.println(result.replace(" ", ""));
+        for (int j = 0; j < text.length(); j++) {
+            textPiece += text.charAt(j);
+
+            for (int i = 0; i < src.length; i++) {
+                if (src[i].contains(textPiece) && textCopy.contains(src[i]))
+                    textCopy = textCopy.replaceAll(src[i], " " + ptr[i] + " ");
+
+                else if (textPiece.contains(src[i])) {
+                    textCopy = textCopy.replaceAll(src[i], " " + ptr[i] + " ");
+                    textPiece = "";
+                    break;
+                }
+            }
+        }
+        System.out.println(textCopy.replace(" ", ""));
 
         // w/ regex
-        result = text;
-        Pattern pattern = Pattern.compile("(?<src>\\w+) (?<ptr>\\w+)");
-        for (int i = 0; i < rlen; i++) {
-            Matcher matcher = pattern.matcher(rules[i]);
-            if (matcher.find()) result = result.
-                    replaceAll(matcher.group("src"),
-                            " " + matcher.group("ptr") + " ");
-        }
-        System.out.println(result.replace(" ", ""));
+        String srcStr = "";
 
+        for (int i = 0; i < rlen; i++) {
+            srcStr += rules[i].split(" ")[0];
+            if (i != rlen - 1) srcStr += "|";
+        }
+
+        HashMap<String, String> replaceMap = new HashMap<>();
+
+        for (String t : rules) replaceMap.put(t.split(" ")[0], t.split(" ")[1]);
+
+        Pattern pattern = Pattern.compile(srcStr);
+        Matcher matcher = pattern.matcher(text);
+        System.out.println(matcher.replaceAll(x -> replaceMap.get(x.group())));
     }
 }
+
 
