5cf7624bf34cddaca0cfc9df2f13895d4c4ee8a4
syberflow <jasoness@yandex.ru>
2020-12-17 13:06:18 +0300

    Done 27-28

---
+++ src/com/netlight/lab27_28/Operation.java
@@ -0,0 +1,9 @@
+package com.netlight.lab27_28;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Operation {
+    String name();
+}
